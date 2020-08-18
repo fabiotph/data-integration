b191582820e8cdb6c47950f03c294c54a2be09d3
fabiotph <fabiotperez@gmail.com>
2020-08-18 00:15:05 -0300

    company fields validation

--- server/src/models/company.go
+++ server/src/models/company.go
@@ -4,6 +4,7 @@ import (
 	"github.com/jinzhu/gorm"
 	uuid "github.com/satori/go.uuid"
 	"log"
+	"regexp"
 )
 
 type Company struct{
@@ -30,4 +31,34 @@ func (company *Company) BeforeCreate(scope *gorm.Scope) error{
 		log.Fatalf("Error during Company Object creating: %v", err)
 	}
 	return err
+}
+
+func (model *CompanyModel) Prepare(company *Company) bool{
+	if !company.validate(){
+		return false
+	}
+	return true
+}
+
+func (model *Company) validate() bool{
+	if(!validateName(model.Name) || !validateZipCode(model.ZipCode)) || !validateWebSite(model.Website){
+		return false
+	}
+	return true
+}
+
+func validateName(name string) bool{
+	isMatch, _ := regexp.MatchString("^([A-ZÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ&']+( [A-ZÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ&'])*)+$", name)
+	return isMatch
+}
+
+func validateZipCode(zipCode string) bool{
+	isMatch, _ := regexp.MatchString("^[0-9]{5}$", zipCode)
+	return isMatch
+}
+
+func validateWebSite(website string) bool{
+	regex := regexp.MustCompile(`^((https?)://[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?((/(([a-z]+[A-Z]*[0-9]*)|([a-z]*[A-Z]+[0-9]*)|([a-z]*[A-Z]*[0-9]+))/?)*|(/?)))?$`)
+	isMatch := regex.MatchString(website)
+	return isMatch
 }
