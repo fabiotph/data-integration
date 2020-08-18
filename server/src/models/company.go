1ca5b7d9e66652a5a040ce3eca36bc2c855111ac
fabiotph <fabiotperez@gmail.com>
2020-08-17 22:38:38 -0300

    GORM library configuration and using the UUID for the Company Id

--- server/src/models/company.go
+++ server/src/models/company.go
@@ -1,5 +1,11 @@
 package models
 
+import (
+	"github.com/jinzhu/gorm"
+	uuid "github.com/satori/go.uuid"
+	"log"
+)
+
 type Company struct{
 	Model
 	Id string `json:"id" gorm:"type:uuid;primary_key"`
@@ -16,4 +22,12 @@ type CompanyModel struct {}
 
 func NewCompanyModel() *CompanyModel {
 	return &CompanyModel{}
+}
+
+func (company *Company) BeforeCreate(scope *gorm.Scope) error{
+	err := scope.SetColumn("Id", uuid.NewV4().String())
+	if err != nil{
+		log.Fatalf("Error during Company Object creating: %v", err)
+	}
+	return err
 }
--- server/src/models/model.go
+++ server/src/models/model.go
@@ -2,9 +2,19 @@ package models
 
 import (
 	"time"
+	"github.com/jinzhu/gorm"
+	"log"
 )
 
 type Model struct {
 	CreatedAt time.Time `json:"-"`
 	UpdatedAt time.Time `json:"-"`
+}
+
+func (model *Model) BeforeCreate(scope *gorm.Scope) error{
+	err := scope.SetColumn("CreatedAt", time.Now())
+	if err != nil{
+		log.Fatalf("Error during Company Object creating: %v", err)
+	}
+	return err
 }
